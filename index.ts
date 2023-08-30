c2e1c6cce1570454c044638f43323f40026428e2
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 00:56:18 +0300

    fin

--- index.ts
+++ index.ts
@@ -5,13 +5,13 @@ import { concatMap, delay, mergeMap } from 'rxjs/operators';
 // Note the difference between concatMap and mergeMap. Because concatMap does not subscribe to the next observable until the previous completes, the value from the source delayed by 2000ms will be emitted first. Contrast this with mergeMap which subscribes immediately to inner observables, the observable with the lesser delay (1000ms) will emit, followed by the observable which takes 2000ms to complete.
 
 //emit delay value
-const source = of(2000, 1000);
+const src$ = of(2000, 1000);
 // map value from source into inner observable, when complete emit result and move to next
-const example = source.pipe(
-  concatMap(val => of(`Delayed by: ${val}ms`).pipe(delay(val)))
+const example = src$.pipe(
+  concatMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
 );
 //output: With concatMap: Delayed by: 2000ms, With concatMap: Delayed by: 1000ms
-const subscribe = example.subscribe(val =>
+const subscribe = example.subscribe((val) =>
   console.log(`With concatMap: ${val}`)
 );
 
@@ -20,6 +20,6 @@ const mergeMapExample = source
   .pipe(
     // just so we can log this after the first example has run
     delay(5000),
-    mergeMap(val => of(`Delayed by: ${val}ms`).pipe(delay(val)))
+    mergeMap((val) => of(`Delayed by: ${val}ms`).pipe(delay(val)))
   )
-  .subscribe(val => console.log(`With mergeMap: ${val}`));
+  .subscribe((val) => console.log(`With mergeMap: ${val}`));
